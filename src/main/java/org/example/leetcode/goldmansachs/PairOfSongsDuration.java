6d93d9cf3dce967d259372e9ba0cc0ff886bd524
Indu Gajula <indugajula.prof@gmail.com>
2024-08-21 12:03:11 +0100

    practicing...

--- src/main/java/org/example/leetcode/goldmansachs/PairOfSongsDuration.java
+++ src/main/java/org/example/leetcode/goldmansachs/PairOfSongsDuration.java
@@ -24,8 +24,26 @@ public class PairOfSongsDuration {
         }
         return ans;
     }
+    public static int numPairsDivisibleBy60_(int[] time) {
+
+        HashMap<Integer, Integer> exists = new HashMap<>();
+        int ans = 0;
+        for(int i=0; i<time.length; i++){
+            int rem = time[i] % 60;
+            int target = 60-rem;
+            ans += exists.getOrDefault(target,0);
+            if(rem != 0){
+                exists.put(rem, exists.getOrDefault(rem,0)+1);
+            }else
+            {
+                exists.put(60, exists.getOrDefault(60,0)+1);
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         System.out.println(numPairsDivisibleBy60(new int[]{30,20,150,100,40})); // 3
+        System.out.println(numPairsDivisibleBy60_(new int[]{30,20,150,100,40}));
     }
 }
---
+++ src/main/java/org/example/myinterviews/coderpad/Questions.java
@@ -0,0 +1,21 @@
+package org.example.myinterviews.coderpad;
+
+public class Questions {
+
+    // 1. find missing number in a given array [0,N]
+    // 2. find non-repeating char in a string
+    // 3. find the max length of substring where characters of the substring is non-repeating
+
+    // 1. find minimum positive integer missing
+    // 2. Merge 2 sorted arrays
+    // 3. Find Second Maximum
+
+    // 1. secondSmallest
+    // 2. Maximum Avg String[][] == {{"Student1","150"},
+    //                              {"student2","100"},
+    //                              {"Student1","200"},
+    //                              {"Student2","50"},
+    //                              {"Student3","300"}}
+
+
+}
